f535efadd71f4e26fc582e71b5aa8aa7e8d415d8
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2018-11-16 23:39:12 +0200

    class eg

---
+++ classes/student.ts
@@ -0,0 +1,25 @@
+class Student {
+    fullName: string
+
+    constructor(
+        public firstName,
+        public middleInitial,
+        public lastName) {
+            this.fullName = firstName + " "
+            + middleInitial 
+            + " " 
+            + lastName
+        }
+}
+
+interface Person {
+    firstName: string
+    lastName: string
+}
+
+function greeter(person: Person) {
+    return "Hello, " + person.firstName + " " + person.lastName
+}
+
+var user = new Student("Mannuel", "Mr", "Ferreira")
+document.body.innerHTML = greeter(user)
