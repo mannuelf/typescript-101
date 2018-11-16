9b13b0372a48e7a036e38d752248b80d80a9cb20
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2018-11-16 23:48:26 +0200

    interface eg

--- interfaces/person.ts
+++ interfaces/person.ts
@@ -1,14 +1,17 @@
+// interface can describe objects
+
 interface Person {
     firstName: string
     lastName: string
 }
 
+// no explicit implements clause required, just pass as param
 function greeter(person: Person) {
     return "Hello, " + person.firstName + " " + person.lastName
 }
 
 let user = {
-    firstName: 123,
+    firstName: "Mannuel",
     lastName: "Ferreira"
 }
 
